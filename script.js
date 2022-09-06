ac74a240bfacb2d443c4f2174baff7532dc378c2
val.74k <60100342+valdemort74@users.noreply.github.com>
2022-09-06 23:30:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
-var posX = 0
-var posY = 0
+var posX = 0;
+var posY = 0;
 
-var distance = 50
+var speed = 5;
+
+var direction = 0
+
+
+
+
+
+function loop(){
+        
+
+    if (direction == 1){
+        posX -= speed
+    }
+    else if (direction == 2){
+        posY -= speed
+    }
+    else if (direction == 3){
+        posX += speed
+    }
+    else if (direction == 4){
+        posY += speed
+    }
+
+
+    document.getElementById("snake").style.transform = "translate(" + posX + "px," + posY + "px)";
+
+    setTimeout(loop, 20);
+}
+
+loop();
+
 
 document.addEventListener("keydown", (event) => {
     if (event.keyCode === 37) {
-        console.log("LEFT")
-        posX = posX - distance
-        document.getElementById("snake").style.transform = "translate(" + posX + "px," + posY + "px)";
+        console.log("LEFT");
+        direction = 1
+        
         return;
     }
 });
     
 document.addEventListener("keydown", (event) => {
     if (event.keyCode === 38) {
-        console.log("UP")
-        posY = posY - distance
-        document.getElementById("snake").style.transform = "translate(" + posX + "px," + posY + "px)";
+        console.log("UP");
+        direction = 2
+       
         return;
     }
 });
 
 document.addEventListener("keydown", (event) => {
     if (event.keyCode === 39) {
-        console.log("RIGHT")
-        posX = posX + distance
-        document.getElementById("snake").style.transform = "translate(" + posX + "px," + posY + "px)";
+        console.log("RIGHT");
+        direction = 3
+      
         return;
     }
 });
@@ -33,9 +64,9 @@ document.addEventListener("keydown", (event) => {
 
 document.addEventListener("keydown", (event) => {
     if (event.keyCode === 40) {
-        console.log("DOWN")
-        posY = posY + distance
-        document.getElementById("snake").style.transform = "translate(" + posX + "px," + posY + "px)";
+        console.log("DOWN");
+        direction = 4
+     
         return;
     }
 });
