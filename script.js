c7824853c477be7a30e461fc650c398dde4abee4
val.74k <60100342+valdemort74@users.noreply.github.com>
2022-09-07 19:45:01 +0200

    objectif detection

--- script.js
+++ script.js
@@ -1,35 +1,63 @@
-var posX = 0;
-var posY = 0;
+let score = 0
 
-var speed = 5;
+let posX = 0;
+let posY = 0;
 
-var direction = 0
+let speed = 3;
+
+let direction = 0;
 
 
-
+let oposX = Math.floor(Math.random() * 450);
+let oposY = Math.floor(Math.random() * 450);
 
 
 function loop(){
         
 
     if (direction == 1){
-        posX -= speed
+        posX -= speed;
     }
     else if (direction == 2){
-        posY -= speed
+        posY -= speed;
     }
     else if (direction == 3){
-        posX += speed
+        posX += speed;
     }
     else if (direction == 4){
-        posY += speed
+        posY += speed;
     }
+
 
 
     if (posX > 450){posX -= 450}
     else if (posY > 450){posY -= 450}
     else if (posX < 0){posX -= -450}
     else if (posY < 0){posY -= -450}
+
+    // console.log("position X : ",posX)
+    // console.log("position Y : ",posY)
+    // console.log()
+    // console.log("objectif position X : ",oposX)
+    // console.log("objectif position Y : ",oposY)
+
+
+    if ((oposX-25 <= posX) && (posX <= oposX+25) && (oposY-25 <= posY) && (posY <= oposY+25)){
+        oposX = Math.floor(Math.random() * 450);
+        oposY = Math.floor(Math.random() * 450);
+        
+        
+
+        score += 1;
+        console.log("score = ",score);
+       
+        
+        document.getElementById("score").textContent = score;
+           
+    
+    }
+
+    document.getElementById("objectif").style.transform = "translate(" + oposX + "px," + oposY + "px)";
 
     document.getElementById("snake").style.transform = "translate(" + posX + "px," + posY + "px)";
 
