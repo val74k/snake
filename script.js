d969606a5da66e6397c25b1705b11d4c0159b010
val.74k <60100342+valdemort74@users.noreply.github.com>
2022-09-10 10:53:54 +0200

    update score script
    - add last_score
    - add best_score
    - fix speed

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-let score = 0
+let score = 0;
+let last_score = 0;
+let best_score = 0;
 
 let posX = 0;
 let posY = 0;
 
 let speed = 3;
+let running = speed;
 
 let direction = 0;
 
@@ -15,116 +18,130 @@ let oposY = Math.floor(Math.random() * 450);
 let mode = 0;
 let modename = "hard";
 
+let div_name = 1;
 
+function godmode() {
+  speed = 5;
 
-
-
-function godmode(){
-    speed = 5
-
-    if (posX > 450){posX -= 450}
-    else if (posY > 450){posY -= 450}
-    else if (posX < 0){posX -= -450}
-    else if (posY < 0){posY -= -450}
-
+  if (posX > 450) {
+    posX -= 450;
+  } else if (posY > 450) {
+    posY -= 450;
+  } else if (posX < 0) {
+    posX -= -450;
+  } else if (posY < 0) {
+    posY -= -450;
+  }
 }
 
-function clear(){
-    posY = 0;
-    posX = 0;
-    score = 0;
+function clear() {
+  if (score > best_score) {
+    best_score = score;
+  }
+  last_score = score;
+  running = 0;
+  posY = 0;
+  posX = 0;
+  speed = 3;
+  score = 0;
+  document.getElementById("last_score").textContent = last_score;
+  document.getElementById("score").textContent = score;
+  document.getElementById("best_score").textContent = best_score;
+}
+
+function loop() {
+  if (direction == 1) {
+    posX -= running;
+  } else if (direction == 2) {
+    posY -= running;
+  } else if (direction == 3) {
+    posX += running;
+  } else if (direction == 4) {
+    posY += running;
+  }
+
+  if (posX > 450) {
+    clear();
+  } else if (posY > 450) {
+    clear();
+  } else if (posX < 0) {
+    clear();
+  } else if (posY < 0) {
+    clear();
+  }
+
+  // console.log("position X : ",posX)
+  // console.log("position Y : ",posY)
+  // console.log()
+  // console.log("objectif position X : ",oposX)
+  // console.log("objectif position Y : ",oposY)
+
+  if (
+    oposX - margin <= posX &&
+    posX <= oposX + margin &&
+    oposY - margin <= posY &&
+    posY <= oposY + margin
+  ) {
+    oposX = Math.floor(Math.random() * 450);
+    oposY = Math.floor(Math.random() * 450);
+
+    score += 1;
+    speed += 1;
+    console.log("score = ", score);
     document.getElementById("score").textContent = score;
-}
 
-function loop(){
+    div_name += 1;
+    var new_div = document.createElement("div");
+    new_div.className = "test";
+  }
 
-    
+  document.getElementById("objectif").style.transform =
+    "translate(" + oposX + "px," + oposY + "px)";
 
+  document.getElementById("snake").style.transform =
+    "translate(" + posX + "px," + posY + "px)";
 
-    if (direction == 1){
-
-        posX -= speed;
-    }
-    else if (direction == 2){
-        posY -= speed;
-    }
-    else if (direction == 3){
-        posX += speed;
-    }
-    else if (direction == 4){
-        posY += speed;
-    }
-
-
-    if (posX > 450){clear()}
-    else if (posY > 450){clear()}
-    else if (posX < 0){clear()}
-    else if (posY < 0){clear()}
-
-   
-    // console.log("position X : ",posX)
-    // console.log("position Y : ",posY)
-    // console.log()
-    // console.log("objectif position X : ",oposX)
-    // console.log("objectif position Y : ",oposY)
-
-
-    if ((oposX-margin <= posX) && (posX <= oposX+margin) && (oposY-margin <= posY) && (posY <= oposY+margin)){
-        oposX = Math.floor(Math.random() * 450);
-        oposY = Math.floor(Math.random() * 450);
-
-        score += 1;
-        console.log("score = ",score);
-       
-        document.getElementById("score").textContent = score;
-           
-    
-    }
-
-    document.getElementById("objectif").style.transform = "translate(" + oposX + "px," + oposY + "px)";
-
-    document.getElementById("snake").style.transform = "translate(" + posX + "px," + posY + "px)";
-
-    setTimeout(loop, 20);
+  setTimeout(loop, 20);
 }
 
 loop();
 
+document.addEventListener("keydown", (event) => {
+  if (event.keyCode === 37) {
+    console.log("LEFT");
+    running = speed;
+    direction = 1;
 
-document.addEventListener("keydown", (event) => {
-    if (event.keyCode === 37) {
-        console.log("LEFT");
-        direction = 1
-        
-        return;
-    }
-});
-    
-document.addEventListener("keydown", (event) => {
-    if (event.keyCode === 38) {
-        console.log("UP");
-        direction = 2
-       
-        return;
-    }
+    return;
+  }
 });
 
 document.addEventListener("keydown", (event) => {
-    if (event.keyCode === 39) {
-        console.log("RIGHT");
-        direction = 3
-      
-        return;
-    }
+  if (event.keyCode === 38) {
+    console.log("UP");
+    running = speed;
+    direction = 2;
+
+    return;
+  }
 });
-    
 
 document.addEventListener("keydown", (event) => {
-    if (event.keyCode === 40) {
-        console.log("DOWN");
-        direction = 4
-     
-        return;
-    }
+  if (event.keyCode === 39) {
+    console.log("RIGHT");
+    running = speed;
+    direction = 3;
+
+    return;
+  }
 });
-    
+
+document.addEventListener("keydown", (event) => {
+  if (event.keyCode === 40) {
+    console.log("DOWN");
+    running = speed;
+    direction = 4;
+
+    return;
+  }
+});
