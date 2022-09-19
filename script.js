56f2997557310178f0f03c06e998777958001d04
valdemort74 <vleconte74@gmail.com>
2022-09-19 22:56:08 +0200

    change border color and try cookie storage

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
+
+ if (document.cookie.length > 0){
+  let score_tablecookie = document.cookie.split(';');
+
+
+
+
+  for (var i=0;i<score_tablecookie.length;i++){
+          if(score_tablecookie[i].indexOf("last_score=") != -1){
+            last_score = score_tablecookie[i].substring(last_score.length + score_tablecookie[i].indexOf(last_score), score_tablecookie[i].length);
+          }
+          if(score_tablecookie[i].indexOf("best_score=") != -1){
+            best_score = score_tablecookie[i].substring(best_score.length + score_tablecookie[i].indexOf(best_score), score_tablecookie[i].length);
+          }
+  }
+  document.getElementById("last_score").textContent = last_score;
+  document.getElementById("best_score").textContent = best_score;
+}
+
+
+
+
+
+
+console.log("best score : ", best_score)
+console.log("last score : ", last_score)
+
+
 let score = 0;
-let last_score = 0;
-let best_score = 0;
+
 
 let posX = 0;
 let posY = 0;
@@ -10,46 +37,66 @@ let running = speed;
 
 let direction = 0;
 
-let margin = 45;
+let margin = 50;
 
 let oposX = Math.floor(Math.random() * 450);
 let oposY = Math.floor(Math.random() * 450);
 
 let mode = 0;
-let modename = "hard";
+
 
 let div_name = 1;
 
-function godmode() {
-  speed = 5;
+// function godmode() {
+//   speed = 5;
 
-  if (posX > 450) {
-    posX -= 450;
-  } else if (posY > 450) {
-    posY -= 450;
-  } else if (posX < 0) {
-    posX -= -450;
-  } else if (posY < 0) {
-    posY -= -450;
-  }
-}
+//   if (posX > 450) {
+//     posX -= 450;
+//   } else if (posY > 450) {
+//     posY -= 450;
+//   } else if (posX < 0) {
+//     posX -= -450;
+//   } else if (posY < 0) {
+//     posY -= -450;
+//   }
+// }
 
 function clear() {
+  
+  
   if (score > best_score) {
+    
     best_score = score;
+  
   }
   last_score = score;
+
+  score = 0
+
   running = 0;
   posY = 0;
   posX = 0;
   speed = 3;
-  score = 0;
+
+  console.log("posX : " ,posX)
+  console.log("posY : " ,posY)
+
+
   document.getElementById("last_score").textContent = last_score;
   document.getElementById("score").textContent = score;
   document.getElementById("best_score").textContent = best_score;
+  
+  document.cookie = "last_score =" + last_score
+  document.cookie = "best_score =" + best_score
+
+
+
 }
 
 function loop() {
+
+  
+
   if (direction == 1) {
     posX -= running;
   } else if (direction == 2) {
@@ -61,13 +108,13 @@ function loop() {
   }
 
   if (posX > 450) {
-    clear();
+      clear();
   } else if (posY > 450) {
-    clear();
+      clear();
   } else if (posX < 0) {
-    clear();
+      clear();
   } else if (posY < 0) {
-    clear();
+      clear();
   }
 
   // console.log("position X : ",posX)
@@ -113,31 +160,46 @@ document.addEventListener("keydown", (event) => {
     direction = 1;
 
     return;
-  }
-});
-
-document.addEventListener("keydown", (event) => {
-  if (event.keyCode === 38) {
+  } else if (event.keyCode === 38) {
     console.log("UP");
     running = speed;
     direction = 2;
 
     return;
-  }
-});
-
-document.addEventListener("keydown", (event) => {
-  if (event.keyCode === 39) {
+  } else if (event.keyCode === 39) {
     console.log("RIGHT");
     running = speed;
     direction = 3;
 
     return;
-  }
-});
+  } else if (event.keyCode === 40) {
+    console.log("DOWN");
+    running = speed;
+    direction = 4;
 
-document.addEventListener("keydown", (event) => {
-  if (event.keyCode === 40) {
+    return;
+  }
+  //            ZQSD
+  //
+  else if (event.keyCode === 81) {
+    console.log("LEFT");
+    running = speed;
+    direction = 1;
+
+    return;
+  } else if (event.keyCode === 90) {
+    console.log("UP");
+    running = speed;
+    direction = 2;
+
+    return;
+  } else if (event.keyCode === 68) {
+    console.log("RIGHT");
+    running = speed;
+    direction = 3;
+
+    return;
+  } else if (event.keyCode === 83) {
     console.log("DOWN");
     running = speed;
     direction = 4;
