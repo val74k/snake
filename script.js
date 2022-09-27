e79dd5d0e3ff08226e039e17985426cb19595438
valdemort74 <vleconte74@gmail.com>
2022-09-27 23:43:42 +0200

    create arrows buttons

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
-
- if (document.cookie.length > 0){
-  let score_tablecookie = document.cookie.split(';');
-
-
-
-
-  for (var i=0;i<score_tablecookie.length;i++){
-          if(score_tablecookie[i].indexOf("last_score=") != -1){
-            last_score = score_tablecookie[i].substring(last_score.length + score_tablecookie[i].indexOf(last_score), score_tablecookie[i].length);
-          }
-          if(score_tablecookie[i].indexOf("best_score=") != -1){
-            best_score = score_tablecookie[i].substring(best_score.length + score_tablecookie[i].indexOf(best_score), score_tablecookie[i].length);
-          }
+if (document.cookie.length > 0) {
+  let score_tablecookie = document.cookie.split(";");
+  for (var i = 0; i < score_tablecookie.length; i++) {
+    if (score_tablecookie[i].indexOf("last_score=") != -1) {
+      last_score = score_tablecookie[i].substring(
+        last_score.length + score_tablecookie[i].indexOf(last_score),
+        score_tablecookie[i].length
+      );
+    }
+    if (score_tablecookie[i].indexOf("best_score=") != -1) {
+      best_score = score_tablecookie[i].substring(
+        best_score.length + score_tablecookie[i].indexOf(best_score),
+        score_tablecookie[i].length
+      );
+    }
   }
   document.getElementById("last_score").textContent = last_score;
   document.getElementById("best_score").textContent = best_score;
 }
 
-
-
-
-
-
-console.log("best score : ", best_score)
-console.log("last score : ", last_score)
-
+console.log("best score : ", best_score);
+console.log("last score : ", last_score);
 
 let score = 0;
-
 
 let posX = 0;
 let posY = 0;
@@ -44,8 +38,23 @@ let oposY = Math.floor(Math.random() * 450);
 
 let mode = 0;
 
-
 let div_name = 1;
+
+
+
+
+// console.log(navigator.userAgent);
+// navigator = navigator.userAgent;
+
+// if (navigator.includes("Firefox")) {
+//   speed += 5;
+//   console.log("firefox");
+// } else if (navigator.includes("Chrome")) {
+//   console.log("chrome");
+// }
+
+
+
 
 // function godmode() {
 //   speed = 5;
@@ -62,41 +71,31 @@ let div_name = 1;
 // }
 
 function clear() {
-  
-  
   if (score > best_score) {
-    
     best_score = score;
-  
   }
   last_score = score;
 
-  score = 0
+  score = 0;
 
   running = 0;
   posY = 0;
   posX = 0;
   speed = 3;
 
-  console.log("posX : " ,posX)
-  console.log("posY : " ,posY)
-
+  console.log("posX : ", posX);
+  console.log("posY : ", posY);
 
   document.getElementById("last_score").textContent = last_score;
   document.getElementById("score").textContent = score;
+  document.getElementById("score_center").textContent = score;
   document.getElementById("best_score").textContent = best_score;
-  
-  document.cookie = "last_score =" + last_score
-  document.cookie = "best_score =" + best_score
 
-
-
+  document.cookie = "last_score =" + last_score;
+  document.cookie = "best_score =" + best_score;
 }
 
 function loop() {
-
-  
-
   if (direction == 1) {
     posX -= running;
   } else if (direction == 2) {
@@ -108,13 +107,13 @@ function loop() {
   }
 
   if (posX > 450) {
-      clear();
+    clear();
   } else if (posY > 450) {
-      clear();
+    clear();
   } else if (posX < 0) {
-      clear();
+    clear();
   } else if (posY < 0) {
-      clear();
+    clear();
   }
 
   // console.log("position X : ",posX)
@@ -136,6 +135,7 @@ function loop() {
     speed += 1;
     console.log("score = ", score);
     document.getElementById("score").textContent = score;
+    document.getElementById("score_center").textContent = score;
 
     div_name += 1;
     var new_div = document.createElement("div");
@@ -151,7 +151,33 @@ function loop() {
   setTimeout(loop, 20);
 }
 
+
 loop();
+
+function button_top(){
+  console.log("UP");
+  running = speed;
+  direction = 2;
+}
+
+function button_right(){
+  console.log("RIGHT");
+  running = speed;
+  direction = 3;
+}
+
+function button_down(){
+  console.log("DOWN");
+  running = speed;
+  direction = 4;
+}
+
+function button_left(){
+  console.log("LEFT");
+  running = speed;
+  direction = 1;
+}
+
 
 document.addEventListener("keydown", (event) => {
   if (event.keyCode === 37) {
@@ -207,3 +233,4 @@ document.addEventListener("keydown", (event) => {
     return;
   }
 });
+
